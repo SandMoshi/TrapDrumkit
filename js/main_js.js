538baad1cd64af83f3a570c22832bdceca91c9dc
Sand Moshi <sandmoshi@gmail.com>
2016-12-14 06:17:53 -0700

    Added CSS when key is pressed and sound is playing

--- js/main_js.js
+++ js/main_js.js
@@ -1,12 +1,30 @@
 window.addEventListener('keydown',function(e){
- const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	 if(!audio){
-		 console.log('null key pressed..doing nothing');
-		 return;
-	 }
- console.log("const audio= " + audio);
- audio.currentTime = 0;
- audio.play();
+		//Send event (e) to the playSound function
 });
+
+function playSound(e){
+	 const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	 if(!audio){
+			 console.log('null key pressed..doing nothing');
+			 return;
+		 }
+	 console.log("const audio= " + audio);
+	 //--
+	 const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	 key.classList.add("playing");
+	 console.log("const key= " + key);
+	 //--
+	 const keys = document.querySelectorAll(".key");
+	 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+	 //--
+	 audio.currentTime = 0; 
+	 audio.play(); 
+} 
+
+
+function removeTransition(e){
+	if(e.propertyName !== 'transform') return; //only remove transition if it is a transform
+	console.log(this);
+	this.classList.remove("playing"); //remove class and revert to original style
+}
 												
-										
