8c4b25052f851e2773b24be5be9fbb0d30a81114
Sand Moshi <sandmoshi@gmail.com>
2017-02-27 20:41:18 -0700

    Added support for number rows

--- js/main_js.js
+++ js/main_js.js
@@ -9,7 +9,36 @@ window.addEventListener('click',function(e){
 });
 
 function playSound(e){
-	 var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	 
+	 var keyPressed = e.keyCode;
+	//-------------------------------------------------
+	//Convert numeric row keys to numpad
+	try{ // prevents errrors when letters are pressed 
+		 var correspondingKeys = [
+				{'key':'49', 'alternate':'97'},
+				{'key':'50', 'alternate':'98'},
+				{'key':'51', 'alternate':'99'},
+				{'key':'52', 'alternate':'100'},
+				{'key':'53', 'alternate':'101'},
+				{'key':'54', 'alternate':'102'},
+				{'key':'55', 'alternate':'103'},
+				{'key':'56', 'alternate':'104'},
+				{'key':'57', 'alternate':'105'},
+		 ];
+		 var oldKey = "" + e.keyCode + "";
+		 console.log("oldKey = " + oldKey);
+		 var obj = correspondingKeys.filter(function ( obj ){
+			 return obj.key === oldKey;
+		 })[0];
+		  keyPressed = obj.alternate;
+	    console.log("alternate = "+ keyPressed);
+	}
+	catch(TypeError){
+		//If alternate key doesn't exist ignore TypeError that results
+	};
+  //-------------------------------------------------
+	
+	 var audio = document.querySelector(`audio[data-key="${keyPressed}"]`);
 	 //if the keycode doesn't match anything, check to see if element was clicked instead
 	 if(!audio){
 		 		console.log(e); //Show which element was clicked
@@ -23,7 +52,7 @@ function playSound(e){
 	 }
 	 console.log("const audio= " + audio);
 	 //--
-	 var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	 var key = document.querySelector(`.key[data-key="${keyPressed}"]`);
 	 //If the keycode doesn't match, check to see if element was clicked
 	 if (key === null){
 		 //Try getting it based on click target instead of keycode
